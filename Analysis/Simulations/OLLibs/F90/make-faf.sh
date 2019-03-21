947819b7ac3a3a104e9294e52511a36de94498c6
Tulasi Nandan Parashar <tulasinandan@gmail.com>
2019-03-21 15:24:01 -0400

    Changed f2py to f2py3

--- Analysis/Simulations/OLLibs/F90/make-faf.sh
+++ Analysis/Simulations/OLLibs/F90/make-faf.sh
@@ -12,37 +12,37 @@ fi
 cd $F90DIR
 
 if [[ "$MACH" =~ ^(flare) ]]; then
-f2py $(\ls *.f90) -lfftw3 -c -m faf 
+f2py3 $(\ls *.f90) -lfftw3 -c -m faf 
 fi
 
 if [[ "$MACH" =~ ^(goose) ]]; then
 FFTWDIR=/home/tulasi/share/FFTW3/lib
-f2py $(\ls *.f90) -L$FFTWDIR -lfftw3 -c -m faf 
+f2py3 $(\ls *.f90) -L$FFTWDIR -lfftw3 -c -m faf 
 fi
 
 if [[ "$MACH" =~ ^(darter) ]]; then
 module swap PrgEnv-intel PrgEnv-gnu
 FFTWDIR=/opt/fftw/3.3.4.0/x86_64/lib
-f2py $(\ls *.f90) -L$FFTWDIR -lfftw3 -c -m faf
+f2py3 $(\ls *.f90) -L$FFTWDIR -lfftw3 -c -m faf
 fi
 
 if [[ "$MACH" =~ ^(yellowstone) ]]; then
 module load intel
 FFTWDIR=/glade/p/work/tulasi/local/lib
-f2py --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
-#f2py --debug-capi --opt=-free --fcompiler=intelem --compiler=intelem --f90flags="-check all -g -debug all -warn all -stand f08 -traceback" -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
+f2py3 --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
+#f2py3 --debug-capi --opt=-free --fcompiler=intelem --compiler=intelem --f90flags="-check all -g -debug all -warn all -stand f08 -traceback" -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
 rm -f *__genmod.*
 fi
 
 if [[ "$MACH" =~ ^(cheyenne) ]]; then
 ml mpt
 ml fftw
-f2py --build-dir $SCRATCH/f2py/ --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L/glade/p/work/tulasi/local/lib -lfftw3 -c -m faf $(\ls *.f90)
+f2py3 --build-dir $SCRATCH/f2py3/ --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L/glade/p/work/tulasi/local/lib -lfftw3 -c -m faf $(\ls *.f90)
 #FFTWDIR=/glade/p/work/tulasi/local/lib
-#f2py --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L$FFTWDIR -lfftw3 -c -m fafC $(\ls *.f90)
-#f2py --debug-capi --opt=-free --fcompiler=intelem --compiler=intelem --f90flags="-check all -g -debug all -warn all -stand f08 -traceback" -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
+#f2py3 --opt=-free --fcompiler=intelem --f90flags="-O3" --compiler=intelem -L$FFTWDIR -lfftw3 -c -m fafC $(\ls *.f90)
+#f2py3 --debug-capi --opt=-free --fcompiler=intelem --compiler=intelem --f90flags="-check all -g -debug all -warn all -stand f08 -traceback" -L$FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)
 rm -f *__genmod.*
 fi
 
-##echo "f2py $FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)"
+##echo "f2py3 $FFTWDIR -lfftw3 -c -m faf $(\ls *.f90)"
 #mv faf*.so ../lib/.
